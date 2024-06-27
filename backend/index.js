18615c2e0a51b5cd47d06802ed0d393f9427c093
Kryp <abhipok4@gmail.com>
2024-06-27 17:12:26 +0545

    User Profile Update Ready

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,19 @@ import express from "express";
 import * as dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import userRoute from "./routes/userRoute.js";
 import { mongoConn } from "./db/connectMongo.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -14,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
+app.use("/api/user", userRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
---
+++ backend/routes/userRoute.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  followUnfollowUser,
+  getSuggestedUsers,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.get("/suggested", protectRoute, getSuggestedUsers);
+router.post("/update", protectRoute, updateUserProfile);
+
+export default router;
